d8d1b5ee53863c0f18880198fdf873932bd20314
moh raihan romadhoni <126458829+raihan-doni16@users.noreply.github.com>
2023-12-07 17:44:34 +0700

    solve notification issue

---
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -0,0 +1,61 @@
+package com.dicoding.todoapp.ui.detail
+
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.utils.DateConverter
+import com.dicoding.todoapp.utils.TASK_ID
+import com.google.android.material.textfield.TextInputEditText
+
+class DetailTaskActivity : AppCompatActivity() {
+    private lateinit var viewModel: DetailTaskViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_detail)
+
+        //TODO 11 : Show detail task and implement delete action
+        val btnDelete = findViewById<Button>(R.id.btn_delete_task)
+        val detailEdTitle = findViewById<TextInputEditText>(R.id.detail_ed_title)
+        val detailEdDescription = findViewById<TextInputEditText>(R.id.detail_ed_description)
+        val detailEdDueDate = findViewById<TextInputEditText>(R.id.detail_ed_due_date)
+
+        val taskId = intent.getIntExtra(TASK_ID, 0)
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory).get(DetailTaskViewModel::class.java)
+        viewModel.apply {
+            setTaskId(taskId)
+            task.observe(this@DetailTaskActivity) {
+                detailEdTitle.text = Editable.Factory.getInstance().newEditable(it.title)
+                detailEdDescription.text =
+                    Editable.Factory.getInstance().newEditable(it.description)
+                detailEdDueDate.text = Editable.Factory.getInstance()
+                    .newEditable(DateConverter.convertMillisToString(it.dueDateMillis))
+            }
+            showToast.observe(this@DetailTaskActivity){event ->
+                val message =event.getContentIfNotHandled()
+                message?.let {
+                    Toast.makeText(this@DetailTaskActivity, it, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            deleteTask.observe(this@DetailTaskActivity){event ->
+                val savedContent =event.getContentIfNotHandled()
+                savedContent?.let {
+                    if (it){
+                        viewModel.task.removeObservers(this@DetailTaskActivity)
+                        finish()
+                    }
+                }
+            }
+        }
+        btnDelete.setOnClickListener {
+            viewModel.deleteTask()
+        }
+    }
+}
---
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskViewModel.kt
@@ -0,0 +1,41 @@
+package com.dicoding.todoapp.ui.detail
+
+import androidx.lifecycle.*
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
+import com.dicoding.todoapp.utils.Event
+import kotlinx.coroutines.launch
+
+class DetailTaskViewModel(private val taskRepository: TaskRepository): ViewModel() {
+
+    private val _taskId = MutableLiveData<Int>()
+
+    private val _task = _taskId.switchMap { id ->
+        taskRepository.getTaskById(id)
+    }
+    val task: LiveData<Task> = _task
+
+    private val _showToast = MutableLiveData<Event<Int>>()
+    val showToast: LiveData<Event<Int>> = _showToast
+
+    private  val _deleteTask = MutableLiveData<Event<Boolean>>()
+    val deleteTask: LiveData<Event<Boolean>> = _deleteTask
+
+    fun setTaskId(taskId: Int) {
+        if (taskId == _taskId.value) {
+            return
+        }
+        _taskId.value = taskId
+    }
+
+    fun deleteTask() {
+        viewModelScope.launch {
+            _task.value?.let {
+                taskRepository.deleteTask(it)
+                _showToast.value = Event(R.string.delete_task)
+                _deleteTask.value = Event(true)
+            }
+        }
+    }
+}
